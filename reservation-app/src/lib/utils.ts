0261448da392e02fd899cfd73bcd9101d5d65d12
b0uch3r <boucher.kilian.pro@gmail.com>
2026-03-22 16:13:46 +0100

    quai ouest v24

--- reservation-app/src/lib/utils.ts
+++ reservation-app/src/lib/utils.ts
@@ -1,18 +1,50 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import { format, parseISO } from 'date-fns'
-import { fr } from 'date-fns/locale'
+
+const DATE_ONLY_RE = /^(\d{4})-(\d{2})-(\d{2})$/
+
+const frenchDateFormatter = new Intl.DateTimeFormat('fr-FR', {
+  day: '2-digit',
+  month: 'short',
+  year: 'numeric',
+})
+
+const frenchTimeFormatter = new Intl.DateTimeFormat('fr-FR', {
+  hour: '2-digit',
+  minute: '2-digit',
+  hourCycle: 'h23',
+})
+
+function parseDateValue(value: string) {
+  const dateOnlyMatch = DATE_ONLY_RE.exec(value)
+
+  if (dateOnlyMatch) {
+    const [, year, month, day] = dateOnlyMatch
+    return new Date(Number(year), Number(month) - 1, Number(day))
+  }
+
+  return new Date(value)
+}
+
+function normalizeIntlSpacing(value: string) {
+  return value.replace(/\u202f/g, ' ').replace(/\u00a0/g, ' ')
+}
+
+function formatFrenchDate(date: Date) {
+  return normalizeIntlSpacing(frenchDateFormatter.format(date))
+}
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export function formatDate(date: string) {
-  return format(parseISO(date), 'dd MMM yyyy', { locale: fr })
+  return formatFrenchDate(parseDateValue(date))
 }
 
 export function formatDateTime(date: string) {
-  return format(parseISO(date), "dd MMM yyyy 'à' HH:mm", { locale: fr })
+  const parsedDate = parseDateValue(date)
+  return `${formatFrenchDate(parsedDate)} à ${normalizeIntlSpacing(frenchTimeFormatter.format(parsedDate))}`
 }
 
 export function formatCurrency(cents: number | null) {
